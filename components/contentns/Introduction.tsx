4e40d7622b07d1972d2fa4ef2e1f8a3fae73d2a4
enomoto-catallaxy <t.enomoto@catallaxy.me>
2022-12-30 01:09:22 +0900

    intrdoduction in contents

---
+++ components/contentns/Introduction.tsx
@@ -0,0 +1,6 @@
+import { memo } from "react";
+import { CommonBox } from "../common/Box";
+
+export const ContensIntroduction = memo(() => {
+  return <CommonBox children="主に技術記事について投稿しています。" />;
+});
